7ef5014ab23621b47292cd725f0875ed7f5feb3c
Ash - SIlverless <ash@silverless.co.uk>
2019-10-25 15:38:36 +0100

    DEV - footer leaders into rows

--- template-parts/footer-leaders.php
+++ template-parts/footer-leaders.php
@@ -6,39 +6,46 @@
         </a>
         
         <div class="container cols-4">
+            
+        <?php if( have_rows('top_row', 'options') ):
+            while ( have_rows('top_row', 'options') ) : the_row();?>
             <div class="col">
-    <?php if( have_rows('left_column', 'options') ):
-        while ( have_rows('left_column', 'options') ) : the_row();?>
-            <div class="item">
+                <div class="item">
                 <i class="<?php the_sub_field('icon');?>"></i>
                 <h3 class="heading heading__sm heading__brand-font heading__caps"><?php the_sub_field('heading');?></h3>        
                 <p><?php the_sub_field('copy');?></p>
                 <a href="<?php the_sub_field('button_target');?>" class="inline">More <span>&xrarr;</span></a>
             </div>
-    <?php endwhile; endif;?>
             </div>
-            <div class="col">
-                <?php if( have_rows('center_column', 'options') ):
-                    while ( have_rows('center_column', 'options') ) : the_row();?>
+        <?php endwhile; endif;?>
+        </div>
+        
+        <div class="container cols-4">        
+            <?php if( have_rows('second_row', 'options') ):
+                while ( have_rows('second_row', 'options') ) : the_row();?>
+                    <div class="col">
                         <div class="item">
                             <i class="<?php the_sub_field('icon');?>"></i>
                             <h3 class="heading heading__sm heading__brand-font heading__caps"><?php the_sub_field('heading');?></h3>
                             <p><?php the_sub_field('copy');?></p>
                             <a href="<?php the_sub_field('button_target');?>" class="inline">More <span>&xrarr;</span></a>
                         </div>
+                    </div>
                 <?php endwhile; endif;?>
-            </div>
-            <div class="col">
-                <?php if( have_rows('right_column', 'options') ):
-                    while ( have_rows('right_column', 'options') ) : the_row();?>
+        </div>    
+            
+        <div class="container cols-4">            
+                <?php if( have_rows('third_row', 'options') ):
+                    while ( have_rows('third_row', 'options') ) : the_row();?>
+                    <div class="col">
                         <div class="item">
                             <i class="<?php the_sub_field('icon');?>"></i>
                             <h3 class="heading heading__sm heading__brand-font heading__caps"><?php the_sub_field('heading');?></h3>
                             <p><?php the_sub_field('copy');?></p>
                             <a href="<?php the_sub_field('button_target');?>" class="inline">More <span>&xrarr;</span></a>
                         </div>
+                    </div>       
                 <?php endwhile; endif;?>
-            </div>        
-    </div>
+        </div>
 
 </div>
